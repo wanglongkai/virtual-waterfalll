9a0fb51bf80b08d42968a7a00cac6b182e5c84d2
wanglongkai <13699283312@163.com>
2024-05-17 09:40:16 +0800

    feat: 虚拟流式滚动实现

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,75 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  流式虚拟滚动
-  <HelloWorld  />
+  <div class="app">
+    <div class="container" ref="fContainerRef">
+      <BookVirtualWaterfall :request="getData" :gap="15" :page-size="20" :column="column" :enter-size="column * 2">
+        <template #item="{ item, imageHeight }">
+          <book-card
+            :detail="{
+              imageHeight,
+              title: item.title,
+              author: item.author,
+              bgColor: item.bgColor,
+              avatar: item.avatar
+            }"
+          />
+        </template>
+      </BookVirtualWaterfall>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from "vue";
+import BookCard from "./components/BookCard.vue";
+import BookVirtualWaterfall from "./components/BookVirtualWaterfall.vue";
+import { ICardItem } from "./components/type";
+import list from "./config/index";
+const fContainerRef = ref<HTMLDivElement | null>(null);
+const column = ref(6);
+const fContainerObserver = new ResizeObserver((entries) => {
+  changeColumn(entries[0].target.clientWidth);
+});
+
+const changeColumn = (width: number) => {
+  if (width > 960) {
+    column.value = 5;
+  } else if (width >= 690 && width < 960) {
+    column.value = 4;
+  } else if (width >= 500 && width < 690) {
+    column.value = 3;
+  } else {
+    column.value = 2;
+  }
+};
+
+onMounted(() => {
+  fContainerRef.value && fContainerObserver.observe(fContainerRef.value);
+});
+
+onUnmounted(() => {
+  fContainerRef.value && fContainerObserver.unobserve(fContainerRef.value);
+});
+
+const getData = (page: number, pageSize: number) => {
+  return new Promise<ICardItem[]>((resolve) => {
+    setTimeout(() => {
+      resolve(list.slice((page - 1) * pageSize, (page - 1) * pageSize + pageSize));
+    }, 1000);
+  });
+};
+</script>
+
+<style scoped lang="scss">
+.app {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  .container {
+    width: 1400px;
+    height: 600px;
+    border: 1px solid red;
+  }
+}
 </style>
